cff386f31796f672f9929df3d401a313e6002c33
jonpolbob <jonpolbob@gmail.com>
2015-02-06 14:25:40 +0100

    clear de tout
    voila c'est vide

---
+++ exemple calculs mobiles.py
@@ -0,0 +1,17 @@
+gmarkets.fr
+-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      cagibi
+# programme lisant un csv , filtre la premiere colonne qui contient la date et heure qu'on ne va pas decoder, et ensuite applique une moyenne mobile sur les open
+# avant de l'afficher
+#
+# Created:     27/07/2014
+# Copyright:   (c) cagibi 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+
